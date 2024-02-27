aed986f2a4bb7ea619187f425560dc6aa2bb9527
Your Name <you@example.com>
2024-02-27 19:07:03 +0300

    updated the pubspec.yaml file

--- sema_mama/lib/main.dart
+++ sema_mama/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import './Screens/welcome.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 void main() {
   runApp(const MyApp());
 }
