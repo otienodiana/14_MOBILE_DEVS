41dd3ca0856232e248dacd046167918b55d54cb6
TetaDory <d.rutayisir1@alustudent.com>
2024-02-28 09:37:41 +0200

    read

--- sema_mama/lib/Screens/signup.dart
+++ sema_mama/lib/Screens/signup.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'login.dart';
+import 'logind.dart';
 
 class SignupScreen extends StatelessWidget {
   final usernameController = TextEditingController();
